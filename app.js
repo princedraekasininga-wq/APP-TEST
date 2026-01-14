7dd3bb9004b761642326adee4d4a97e8825e2806
princedraekasininga-wq <princedraekasininga@gmail.com>
2026-01-15 01:10:17 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==========================================
 // APP VERSION CONTROL
 // ==========================================
-const APP_VERSION = "1.7.3"; // Force Refresh for New UI
+const APP_VERSION = "1.7.2"; // Force Refresh for New UI
 
 
 // ==========================================
