1114122dcbdca05812f2be55c8614c4a897a8ea3
princ <princedraekasininga@gmail.com>
2026-03-06 00:57:18 +0200

    Updated app version to 2.8.5 in app-config.js

--- shared/app-config.js
+++ shared/app-config.js
@@ -7,7 +7,7 @@
   // ---------------------------
   // 1) Versioning
   // ---------------------------
-  cfg.version = cfg.version || "2.8.2";
+  cfg.version = cfg.version || "2.8.";
   g.STALLZ_APP_VERSION = cfg.version;
 
   // ---------------------------
